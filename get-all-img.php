53f34c9f25bbadaa3d830a676a82db1d4f3b565a
DNSCond <condi7002@gmail.com>
2026-09-26 10:57:42 +0200

    yesterday

---
+++ get-all-img.php
@@ -0,0 +1,4 @@
+<?php header('content-type: application/json'); $data = array('images' => array());
+foreach (glob('imgdata/*.*') as $echo) if (preg_match('/([^\\/]+)$/D', $echo,
+$matches)) if(!str_starts_with($matches[1], '.')) $data['images'][ ] = $echo;
+echo json_encode($data);
---
+++ htignore/diff-server.py
@@ -0,0 +1,17 @@
+import requests
+
+local = requests.get('http://localhost/gallery/get-all-img.php')
+server = requests.get('https://antrequest.nl/gallery/get-all-img.php')
+
+print('local:', local.status_code, 'server status:', server.status_code)
+
+l = set(local.json()['images'])
+s = set(server.json()['images'])
+
+print('server but not local:')
+print('   ', '\n    '.join(s - l))
+
+print('local but not server:')
+print('   ', '\n    '.join(l - s))
+
+input('enter to exit:')
